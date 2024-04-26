e3945595a9faac49b40e878499fc0de31cde11dd
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-26 18:06:02 -0500

    ansible

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -5,7 +5,11 @@
   tasks: 
   - name: Check user exists or not
     ansible.builtin.command: id expense
+    register: USER
     ignore_errors: true
+  - name: print user info
+    ansible.builtin.debug:
+        msg: "User= {{USER}}"  
   - name: say Hello
     ansible.builtin.debug:
         msg: "Hello"
