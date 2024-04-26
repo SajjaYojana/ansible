f77df96a6545beadb244ef687a0215d6b98d7e27
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-26 18:11:07 -0500

    ansible

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -1,20 +1,20 @@
-
-- name: Conditions
+- name: conditions
   hosts: web
   become: yes
-  tasks: 
-  - name: Check user exists or not
+  tasks:
+  - name: check user exists or not
     ansible.builtin.command: id expense
     register: USER
     ignore_errors: true
-  - name: print user info
+
+  - name: print user information
     ansible.builtin.debug:
-        msg: "User= {{USER}}"
+      msg: "User info: {{USER}}"
 
   - name: create user
-    ansible.builtcommand: useradd expense
-    when: USER.rc != 0 #condition 
+    ansible.builtin.command: useradd expense
+    when: USER.rc != 0 #condition
 
   - name: say Hello
     ansible.builtin.debug:
-        msg: "Hello"
+      msg: "Hello"
