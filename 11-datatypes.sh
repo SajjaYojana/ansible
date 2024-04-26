baf439aaa558f98d1e2d0682c5e3d863d5cab2d6
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-26 17:51:23 -0500

    ansible

---
+++ 11-datatypes.sh
@@ -0,0 +1,18 @@
+- name: data types
+  hosts: localhost
+    vars:
+        Course: Devops with AWS #string
+        Tools:   #list
+        - Linux
+        - Shell
+        - Ansible
+        Experience:  #map or directory
+            DevOps: 3 #number
+            AWS: 2
+            Docker: 1
+        RealProject: True #boolean
+
+    tasks:
+    - name: Print Variables
+      ansible.builtin.debug:
+        msg: "Course: {{Course}}, Tools covered: {{Tools}}, Experience: {{Experience}}"    
