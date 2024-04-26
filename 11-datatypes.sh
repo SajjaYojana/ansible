58555f4f4fe8b00015d10b5fafc20a25188f8697
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-26 17:55:24 -0500

    ansible

--- 11-datatypes.sh
+++ 11-datatypes.sh
@@ -1,18 +1,17 @@
 - name: data types
   hosts: localhost
   vars:
-    Course: Devops with AWS #string
-    Tools:   #list
+    Course: DevOps with AWS #string
+    Tools: # list
     - Linux
     - Shell
     - Ansible
-    Experience:  #map or directory
-        DevOps: 3 #number
-        AWS: 2
-        Docker: 1
-    RealProject: True #boolean
-
+    Experience: # map or dictionary
+      DevOps: 3 # number
+      AWS: 2
+      Docker: 1
+    RealProject: true #boolean
   tasks:
-  - name: Print Variables
-  ansible.builtin.debug:
-    msg: "Course: {{Course}}, Tools covered: {{Tools}}, Experience: {{Experience}}"    
+  - name: print variables
+    ansible.builtin.debug:
+      msg: "Course: {{Course}}, Tools covered: {{Tools}}, Experience is: {{Experience}}, is real project: {{RealProject}}"
